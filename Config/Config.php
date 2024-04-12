29dbf083b86e818a0fa086af0ab9e47ba1b38600
Luciano Bacab <lucianobacab427@gmail.com>
2024-04-12 17:31:55 -0600

    Sistema Alumnos

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost/boletos/";
+const host = "localhost:3307";
+const user = "root";
+const pass = "1525";
+const db = "biblioteca";
+const charset = "charset=utf8";
+?>
